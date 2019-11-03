a4ce2a56ba160b2d82d52eb469c79a883f991ec6
Eduard <51266843+rabinbalta@users.noreply.github.com>
2019-11-03 16:13:58 +0200

    Add files via upload

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+
+module.exports = {
+	entry: './dev/script.js',
+	output: {
+		path: path.resolve(__dirname, 'site'),
+		filename: 'bundle.js'
+	},
+	mode: 'development',
+	watch: true,
+	devServer: {
+		contentBase: path.resolve(__dirname, 'site'),
+		host: 'localhost',
+		port: 9090,
+	},
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				use: {
+					loader: 'babel-loader',
+					options: {
+						presets: ['@babel/preset-env', '@babel/preset-react']
+					}
+				}
+			}
+		]		
+	}
+}
